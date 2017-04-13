73382eaace3c259de2f89d00885fee678fb7335f
Jonno <jonnowitts@gmail.com>
2017-04-13 21:01:41 +0100

    Added text colour changing button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
+  // [ ] Property binding - Component to Dom (one way up)
+  // ( ) Event binding - Dom to Component
   title = 'Hello World!';
+  name = 'Jonno';
+  jonnosColor = 'red';
+
+  changeTextColor() {
+    this.jonnosColor = this.jonnosColor === 'blue' ? 'red' : 'blue';// If colour is blue, go red, else go blue
+  }
 }
