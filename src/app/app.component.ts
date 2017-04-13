6912e01d28d3e0694da0f7e1d8ffe81cde5f7caa
Jonno <jonnowitts@gmail.com>
2017-04-13 21:25:22 +0100

    Added a list of customers from a TypeScript array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,19 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-
-  // [ ] Property binding - Component to Dom (one way up)
-  // ( ) Event binding - Dom to Component
+  // [ ] means Property binding - Component to Dom (one way up)
+  // ( ) means Event binding - Dom to Component
   title = 'Hello World!';
   name = 'Jonno';
   jonnosColor = 'red';
+  jonnosWeight = 'bold';
+  customers = [
+    {id: 1, name: 'Jonno'},
+    {id: 2, name: 'Ward'},
+    {id: 3, name: 'Kevin'},
+    {id: 4, name: 'Annie'},
+    {id: 5, name: 'Sally'}
+  ];
 
   changeTextColor() {
     this.jonnosColor = this.jonnosColor === 'blue' ? 'red' : 'blue';// If colour is blue, go red, else go blue
